788dc528c5e3c95021b9ed980786fab2f4709219
Rubens Alves <alvesrubens926@gmail.com>
2025-05-19 22:46:29 -0300

    Inicializando projeto dotnet MVC

---
+++ Models/EmprestimosModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmprestimosBook.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimos { get; set; } = DateTime.Now;
+        
+    }
+}

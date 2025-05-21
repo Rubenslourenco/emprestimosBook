d43764a5392c7349161049024b616520c12bf48b
Rubenslourenco <alvesrubens926@gmail.com>
2025-05-20 22:41:07 -0300

    feat: add EmprestimoController e Index do emprestimo

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmprestimosBook.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmprestimosBook.Data
@@ -12,5 +13,8 @@ namespace EmprestimosBook.Data
         {
             
         }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+
     }
 }
--- Models/EmprestimosModel.cs
+++ Models/EmprestimosModel.cs
@@ -9,8 +9,9 @@ namespace EmprestimosBook.Models
     {
         public int Id { get; set; }
         public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
         public string LivroEmprestado { get; set; }
-        public DateTime DataEmprestimos { get; set; } = DateTime.Now;
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
         
     }
 }

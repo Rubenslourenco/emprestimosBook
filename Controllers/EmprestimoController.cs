fb766dee4c9d37cd1b02ccc426a62be986c006c6
Rubenslourenco <alvesrubens926@gmail.com>
2025-05-20 22:59:48 -0300

    feat: imprimindo users na tela

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using EmprestimosBook.Data;
+using EmprestimosBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +14,15 @@ namespace EmprestimosBook.Controllers
     [Route("[controller]")]
     public class EmprestimoController : Controller
     {
-
+        private readonly ApplicationDbContext _db;
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
         }
 
        

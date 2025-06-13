b5758c003f7f276aa1c0a4b0cbf4f51c18e453de
Rubenslourenco <alvesrubens926@gmail.com>
2025-06-13 11:25:51 -0300

    fix : ajustei no layout das pages

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using EmprestimosBook.Data;
 using EmprestimosBook.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace EmprestimosBook.Controllers
 {

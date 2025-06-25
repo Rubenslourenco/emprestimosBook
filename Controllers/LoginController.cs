f02a89606707949c532ee42915e23f754e24391f
Rubenslourenco <alvesrubens926@gmail.com>
2025-06-24 22:45:56 -0300

    feat: add controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimosBook.Controllers
+{
+ 
+    public class LoginController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+ 
+    }
+}

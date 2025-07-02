f5e7111301aefbbc4c697278d2f2b913285bd6d7
Rubenslourenco <alvesrubens926@gmail.com>
2025-07-01 23:13:26 -0300

    feat: add entidade ResponseModel

---
+++ Models/ResponseModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmprestimosBook.Models
+{
+    public class ResponseModel<T>
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;
+        
+    }
+}
